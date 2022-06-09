40d480182f58d35b1d3f6a3a4c165101dfa97a1e
John Paul Medina <jp@leadtrust.io>
2022-06-09 16:04:53 -0400

    Initial commit

---
+++ src/Middleware/CheckAuthCodeRequestMiddleware.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of OAuth 2.0 Laravel.
+ *
+ * (c) Luca Degasperi <packages@lucadegasperi.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nexusvc\OAuth2Server\Middleware;
+
+use Closure;
+use Nexusvc\OAuth2Server\Authorizer;
+
+/**
+ * This is the check auth code request middleware class.
+ *
+ * @author Luca Degasperi <packages@lucadegasperi.com>
+ */
+class CheckAuthCodeRequestMiddleware
+{
+    /**
+     * The authorizer instance.
+     *
+     * @var \Nexusvc\OAuth2Server\Authorizer
+     */
+    protected $authorizer;
+
+    /**
+     * Create a new check auth code request middleware instance.
+     *
+     * @param \Nexusvc\OAuth2Server\Authorizer $authorizer
+     */
+    public function __construct(Authorizer $authorizer)
+    {
+        $this->authorizer = $authorizer;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @param \Closure $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $this->authorizer->setRequest($request);
+
+        $this->authorizer->checkAuthCodeRequest();
+
+        return $next($request);
+    }
+}
